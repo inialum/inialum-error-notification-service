ad887b5987844af293c2228a7123e78df16b74f2
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2025-11-29 23:35:19 +0900

    chore: Add type parameter for Bindings for apiV1

--- app/src/routes/api/v1/index.ts
+++ app/src/routes/api/v1/index.ts
@@ -1,6 +1,7 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
 
 import { notifyApiV1 } from './notify'
+import type { Bindings } from '@/types'
 
-export const apiV1 = new OpenAPIHono()
+export const apiV1 = new OpenAPIHono<{ Bindings: Bindings }>()
 apiV1.route('/notify', notifyApiV1)
